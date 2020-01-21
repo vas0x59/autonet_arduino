0c9a2c14879cf5e0c28c245527d671aa9f04b187
vas59 <vasily.yuryev.a1@gmail.com>
2020-01-21 22:49:03 +0700

    Code

--- Autonet_Arduino/src/main.cpp
+++ Autonet_Arduino/src/main.cpp
@@ -3,7 +3,9 @@
 #include <Tetrix_Motor.cpp>
 #include <Sharp.h>
 #include <Ping.h>
+#include <Adafruit_PWMServoDriver.h>
 
+//CONFIG
 #define SHARP1_PIN A0
 #define SHARP2_PIN A1
 #define SHARP3_PIN A2
@@ -12,7 +14,13 @@
 #define PING1_PIN 2
 #define PING2_PIN 3
 
-MotorDriver driver(2);
+#define DRIVER_ADDRES 2
+
+// #define SERVO1_PIN
+
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+MotorDriver driver(DRIVER_ADDRES);
 
 Sharp sharp1(SHARP1_PIN);
 Sharp sharp2(SHARP2_PIN);
@@ -34,6 +42,11 @@ long enc1 = 0;
 long enc2 = 0;
 float bat = 0;
 
+float m1 = 0;
+float m2 = 0;
+
+bool emergency = false;
+
 void read_sensors()
 {
     //Sharps
@@ -54,11 +67,29 @@ void read_driver()
     bat = driver.read_bat();
 }
 
+void send_to_motors()
+{
+    if (!emergency)
+    {
+        driver.set_speed_m1(m1);
+        driver.set_speed_m2(m2);
+    }
+}
+
 void setup()
 {
+    Serial.begin(115200);
     Wire.begin();
     driver.init();
-    Serial.begin(115200);
+    pwm.begin();
+
+    pwm.setOscillatorFrequency(27000000);
+    pwm.setPWMFreq(1600); // This is the maximum PWM frequency
+
+    // if you want to really speed stuff up, you can go into 'fast 400khz I2C' mode
+    // some i2c devices dont like this so much so if you're sharing the bus, watch
+    // out for this!
+    // Wire.setClock(400000);
 }
 
 void loop()
