48c811920c1a2e64a77213af0807e71f91242f8f
vas59 <vasily.yuryev.a1@gmail.com>
2020-01-22 21:26:24 +0700

    scan

--- Autonet_Arduino/src/main.cpp
+++ Autonet_Arduino/src/main.cpp
@@ -13,8 +13,8 @@
 #include <std_msgs/Int16.h>
 #include <std_msgs/Int32.h>
 
-#include <Servo.h>
 #include <Encoder.h>
+#include <Servo.h>
 // создаём объекты для управления сервоприводами
 Servo s1;
 Servo s2;
@@ -164,12 +164,11 @@ void send_to_servos() {
     s3.write(servo3);
     s4.write(servo4);
 
-  }
-  else {
+  } else {
     s1.write(servo1_first);
     s2.write(servo2_first);
     s3.write(servo3_first);
-    s4.write(servo4_first);  
+    s4.write(servo4_first);
   }
 }
 // OUT
@@ -410,65 +409,41 @@ void loop() {
   send_to_servos();
 }
 
+void scan_i2c() {
+  while (true) {
+    byte error, address;
+    int nDevices;
 
-/*
+    Serial.println("Scanning...");
 
-#include <Arduino.h>
+    nDevices = 0;
+    for (address = 1; address < 127; address++) {
+      // The i2c_scanner uses the return value of
+      // the Write.endTransmisstion to see if
+      // a device did acknowledge to the address.
+      Wire.beginTransmission(address);
+      error = Wire.endTransmission();
 
-#include <Wire.h>
- 
- 
-void setup()
-{
-  Wire.begin();
- 
-  Serial.begin(9600);
-  while (!Serial);             // Leonardo: wait for serial monitor
-  Serial.println("\nI2C Scanner");
-}
- 
- 
-void loop()
-{
-  byte error, address;
-  int nDevices;
- 
-  Serial.println("Scanning...");
- 
-  nDevices = 0;
-  for(address = 1; address < 127; address++ )
-  {
-    // The i2c_scanner uses the return value of
-    // the Write.endTransmisstion to see if
-    // a device did acknowledge to the address.
-    Wire.beginTransmission(address);
-    error = Wire.endTransmission();
- 
-    if (error == 0)
-    {
-      Serial.print("I2C device found at address 0x");
-      if (address<16)
-        Serial.print("0");
-      Serial.print(address,HEX);
-      Serial.println("  !");
- 
-      nDevices++;
+      if (error == 0) {
+        Serial.print("I2C device found at address 0x");
+        if (address < 16)
+          Serial.print("0");
+        Serial.print(address, HEX);
+        Serial.println("  !");
+
+        nDevices++;
+      } else if (error == 4) {
+        Serial.print("Unknown error at address 0x");
+        if (address < 16)
+          Serial.print("0");
+        Serial.println(address, HEX);
+      }
     }
-    else if (error==4)
-    {
-      Serial.print("Unknown error at address 0x");
-      if (address<16)
-        Serial.print("0");
-      Serial.println(address,HEX);
-    }    
+    if (nDevices == 0)
+      Serial.println("No I2C devices found\n");
+    else
+      Serial.println("done\n");
+
+    delay(5000); // wait 5 seconds for next scan
   }
-  if (nDevices == 0)
-    Serial.println("No I2C devices found\n");
-  else
-    Serial.println("done\n");
- 
-  delay(5000);           // wait 5 seconds for next scan
-}*/
-
-
-
+}
